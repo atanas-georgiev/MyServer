dbaf980dad8a6feb6549ead1850df840d59b567e
avg <atanasgeorgiev83@gmail.com>
2016-06-17 22:00:11 +0300

    updated

--- src/Web/MyServer.Web.Main/Scripts/image-details.js
+++ src/Web/MyServer.Web.Main/Scripts/image-details.js
@@ -1,7 +1,41 @@
-﻿function imageDetailsUpdateView() {
+﻿var selectedIds;
+var selectedTitles;
+
+Array.prototype.getUnique = function () {
+    var u = {}, a = [];
+    for (var i = 0, l = this.length; i < l; ++i) {
+        if (u.hasOwnProperty(this[i])) {
+            continue;
+        }
+        a.push(this[i]);
+        u[this[i]] = 1;
+    }
+    return a;
+}
+
+function imageDetailsUpdateView() {
     if ($(".selected")[0]) {
         $("#image-edit-box").show();
     } else {
         $("#image-edit-box").hide();
     }
+
+    selectedIds = $(".selected").map(function () {
+        return $(this).data("id");
+    }).get().getUnique();
+
+    selectedTitles = $(".selected").map(function () {
+        return $(this).data("title");
+    }).get().getUnique();
 };
+
+$(".image-edit-box-title-btn").click(function () {
+    $(".image-edit-box-modal-ids").attr("value", selectedIds);
+    if (selectedTitles.length === 1) {
+        $(".image-edit-box-modal-title").val(selectedTitles);
+    }
+});
+
+
+
+// image-edit-box-title-btn
