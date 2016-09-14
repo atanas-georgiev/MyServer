af1b578e6e89ab559db7c16ec3a7ba7325b59eb1
avg <atanasgeorgiev83@gmail.com>
2016-09-14 20:34:12 +0300

    Added images view in albums

--- src/Web/MyServer.Web/Areas/ImageGalleryAdmin/Models/Image/ImageViewModel.cs
+++ src/Web/MyServer.Web/Areas/ImageGalleryAdmin/Models/Image/ImageViewModel.cs
@@ -9,36 +9,37 @@
     using Microsoft.AspNetCore.Hosting;
     using System.Text;
     using System.Globalization;
+    using Helpers;
 
-    public class ImageViewModel : IMapFrom<Image>//, IHaveCustomMappings
+    public class ImageViewModel : IMapFrom<Image>, IHaveCustomMappings
     {
         public Guid Id { get; set; }
 
-        //public Guid? AlbumId { get; set; }
+        public Guid? AlbumId { get; set; }
 
-        //[MaxLength(50)]
-        //public string Aperture { get; set; }
+        [MaxLength(50)]
+        public string Aperture { get; set; }
 
-        //[MaxLength(50)]
-        //public string CameraMaker { get; set; }
+        [MaxLength(50)]
+        public string CameraMaker { get; set; }
 
-        //[MaxLength(50)]
-        //public string CameraModel { get; set; }
+        [MaxLength(50)]
+        public string CameraModel { get; set; }
 
-        //public DateTime? DateTaken { get; set; }
+        public DateTime? DateTaken { get; set; }
 
-        //[MaxLength(50)]
-        //public string ExposureBiasStep { get; set; }
+        [MaxLength(50)]
+        public string ExposureBiasStep { get; set; }
 
-        //[MaxLength(200)]
-        //public string FileName { get; set; }
+        [MaxLength(200)]
+        public string FileName { get; set; }
 
-        //[MaxLength(50)]
-        //public string FocusLen { get; set; }
+        [MaxLength(50)]
+        public string FocusLen { get; set; }
 
-        //public int Height { get; set; }
+        public int Height { get; set; }
         
-        //public string Info { get; set; }
+        public string Info { get; set; }
 
         //public virtual GpsDataViewModel ImageGpsData { get; set; }
 
@@ -48,108 +49,108 @@
         //[Computed]
         //public List<double> GpsCoordinates => this.ImageGpsData != null ? new List<double>() { this.ImageGpsData.Latitude.Value, this.ImageGpsData.Longitude.Value } : null;
 
-        //[MaxLength(50)]
-        //public string Iso { get; set; }
+        [MaxLength(50)]
+        public string Iso { get; set; }
 
-        //[MaxLength(100)]
-        //public string Lenses { get; set; }
+        [MaxLength(100)]
+        public string Lenses { get; set; }
 
-        //public int LowHeight { get; set; }
-        
-        //public string LowImageSource { get; set; }
+        public int LowHeight { get; set; }
 
-        //public int LowWidth { get; set; }
-        
-        //public string MiddleImageSource { get; set; }
+        public string LowImageSource { get; set; }
 
-        //public int MidHeight { get; set; }
+        public int LowWidth { get; set; }
 
-        //public int MidWidth { get; set; }
+        public string MiddleImageSource { get; set; }
 
-        //public string OriginalDownloadPath { get; set; }
+        public int MidHeight { get; set; }
 
-        //[MaxLength(100)]
-        //public string OriginalFileName { get; set; }
+        public int MidWidth { get; set; }
 
-        //[MaxLength(50)]
-        //public string ShutterSpeed { get; set; }
+        public string OriginalDownloadPath { get; set; }
 
-        //[MaxLength(200)]
-        //public string Title { get; set; }
+        [MaxLength(100)]
+        public string OriginalFileName { get; set; }
 
-        //public int Width { get; set; }
+        [MaxLength(50)]
+        public string ShutterSpeed { get; set; }
 
-        //public void CreateMappings(IMapperConfigurationExpression configuration)
-        //{
-        //   // configuration.CreateMap<Image, ImageViewModel>()
-        //     //   .ForMember(m => m.Info, opt => opt.MapFrom(src => MapStatus(src)));
-        //        //.ForMember(m => m.OriginalDownloadPath, opt => opt.MapFrom(c => this.appEnvironment + Constants.TempContentFolder + "\\" + this.Id + "\\" + this.OriginalFileName))
-        //        //.ForMember(m => m.MiddleImageSource, opt => opt.MapFrom(c => this.appEnvironment + Constants.MainContentFolder + "\\" + this.AlbumId + "\\" + Constants.ImageFolderMiddle + "\\" + this.FileName))
-        //        //.ForMember(m => m.LowImageSource, opt => opt.MapFrom(c => this.appEnvironment + Constants.MainContentFolder + "\\" + this.AlbumId + "\\" + Constants.ImageFolderLow + "\\" + this.FileName));
-        //}
+        [MaxLength(200)]
+        public string Title { get; set; }
 
-        //private string MapStatus(Image source)
-        //{
-        //    var result = new StringBuilder();
+        public int Width { get; set; }
 
-        //    if (!string.IsNullOrEmpty(source.Title))
-        //    {
-        //        result.Append(source.Title + "<br/>");
-        //    }
+        public void CreateMappings(IMapperConfigurationExpression configuration)
+        {
+            configuration.CreateMap<Image, ImageViewModel>()
+                .ForMember(m => m.Info, opt => opt.MapFrom(src => MapStatus(src)))
+                .ForMember(m => m.OriginalDownloadPath, opt => opt.MapFrom(c => Constants.TempContentFolder + "\\" + c.Id + "\\" + c.OriginalFileName))
+                .ForMember(m => m.MiddleImageSource, opt => opt.MapFrom(c => Constants.MainContentFolder + "\\" + c.AlbumId + "\\" + Constants.ImageFolderMiddle + "\\" + c.FileName))
+                .ForMember(m => m.LowImageSource, opt => opt.MapFrom(c => Constants.MainContentFolder + "\\" + c.AlbumId + "\\" + Constants.ImageFolderLow + "\\" + c.FileName));
+        }
 
-        //    result.Append("<small>");
+        static string MapStatus(Image source)
+        {
+            var result = new StringBuilder();
 
-        //    if (!string.IsNullOrEmpty(source.CameraMaker))
-        //    {
-        //        result.Append(source.CameraMaker + " ");
-        //    }
+            if (!string.IsNullOrEmpty(source.Title))
+            {
+                result.Append(source.Title + "<br/>");
+            }
 
-        //    if (!string.IsNullOrEmpty(source.CameraModel))
-        //    {
-        //        result.Append(source.CameraModel + " ");
-        //    }
+            result.Append("<small>");
 
-        //    if (!string.IsNullOrEmpty(source.Lenses))
-        //    {
-        //        result.Append(source.Lenses + " ");
-        //    }
+            if (!string.IsNullOrEmpty(source.CameraMaker))
+            {
+                result.Append(source.CameraMaker + " ");
+            }
 
-        //    result.Append("<br/>");
+            if (!string.IsNullOrEmpty(source.CameraModel))
+            {
+                result.Append(source.CameraModel + " ");
+            }
 
-        //    if (!string.IsNullOrEmpty(source.Aperture))
-        //    {
-        //        result.Append(source.Aperture + " ");
-        //    }
+            if (!string.IsNullOrEmpty(source.Lenses))
+            {
+                result.Append(source.Lenses + " ");
+            }
 
-        //    if (!string.IsNullOrEmpty(source.ShutterSpeed))
-        //    {
-        //        result.Append(source.ShutterSpeed + " ");
-        //    }
+            result.Append("<br/>");
 
-        //    if (source.FocusLen != null)
-        //    {
-        //        result.Append(source.FocusLen + " ");
-        //    }
+            if (!string.IsNullOrEmpty(source.Aperture))
+            {
+                result.Append(source.Aperture + " ");
+            }
 
-        //    if (source.Iso != null)
-        //    {
-        //        result.Append("ISO " + source.Iso + " ");
-        //    }
+            if (!string.IsNullOrEmpty(source.ShutterSpeed))
+            {
+                result.Append(source.ShutterSpeed + " ");
+            }
 
-        //    if (source.ExposureBiasStep != null)
-        //    {
-        //        result.Append(source.ExposureBiasStep);
-        //    }
+            if (source.FocusLen != null)
+            {
+                result.Append(source.FocusLen + " ");
+            }
 
-        //    if (source.DateTaken != null)
-        //    {
-        //        result.Append(
-        //            "<br/>" + source.DateTaken.Value.ToString("dd-MMM-yy"));
-        //    }
+            if (source.Iso != null)
+            {
+                result.Append("ISO " + source.Iso + " ");
+            }
 
-        //    result.Append("</small>");
+            if (source.ExposureBiasStep != null)
+            {
+                result.Append(source.ExposureBiasStep);
+            }
 
-        //    return result.ToString();
-        //}
+            if (source.DateTaken != null)
+            {
+                result.Append(
+                    "<br/>" + source.DateTaken.Value.ToString("dd-MMM-yy"));
+            }
+
+            result.Append("</small>");
+
+            return result.ToString();
+        }
     }
 }
--- src/Web/MyServer.Web/Helpers/PathHelper.cs
+++ src/Web/MyServer.Web/Helpers/PathHelper.cs
@@ -6,7 +6,9 @@ namespace MyServer.Web.Helpers
     public class PathHelper
     {
         public static string WwwPath = string.Empty;
-        
+
+        public static string WwwRootPath = string.Empty;
+
         public PathHelper(IHostingEnvironment appEnvironment)
         {
             WwwPath = appEnvironment.WebRootPath;
