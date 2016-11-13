28996b2ed294f620407ef9529aae7a7910765773
avg <atanasgeorgiev83@gmail.com>
2016-11-13 16:46:21 +0200

    Added missing areas and views.

---
+++ src/Web/MyServer.Web/Areas/FileManager/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MyServer.Data;
+using MyServer.Data.Models;
+using MyServer.Services.Users;
+using MyServer.Web.Areas.Shared.Controllers;
+
+namespace MyServer.Web.Areas.FileManager.Controllers
+{
+    [Area("FileManager")]
+    public class HomeController : BaseController
+    {
+        public HomeController(IUserService userService, UserManager<User> userManager, SignInManager<User> signInManager, MyServerDbContext dbContext) : base(userService, userManager, signInManager, dbContext)
+        {
+        }
+
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
---
+++ src/Web/MyServer.Web/Controllers/About.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyServer.Web.Controllers
+{
+    public class About : Controller
+    {
+        public IActionResult AboutMe()
+        {
+            return this.View();
+        }
+
+        public IActionResult CV()
+        {
+            return this.View();
+        }
+
+        public IActionResult Contacts()
+        {
+            return this.View();
+        }
+
+        public IActionResult MyProjects()
+        {
+            return this.View();
+        }
+    }
+}
