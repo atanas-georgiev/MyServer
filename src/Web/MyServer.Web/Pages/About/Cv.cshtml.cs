54387a20b3dde13efc5fbc3e8d7256a161538566
nasko <atanasgeorgiev83@gmail.com>
2017-10-15 11:30:18 +0300

    Added CV page

---
+++ src/Web/MyServer.Web/Pages/About/Cv.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyServer.Web.Pages.About
+{
+    public class CvModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
